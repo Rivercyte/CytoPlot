25bd5dcf464277edd721f4f9008e76978885fc30
Paul Müller <dev@craban.de>
2024-09-13 18:45:43 +0200

    ci: make release version

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 set -e
 set -x
 
+# set default release to 1.1.1.1
+RELEASE_VERSION="${RELEASE_VERSION:-1.1.1.1}"
+if [[ "$RELEASE_VERSION" =~ "main" ]]; then
+  $RELEASE_VERSION="1.1.1.1"
+fi
 
 # override version
-RELEASE_VERSION="${RELEASE_VERSION:-1.1.1.1}"  # set default release
 python write_version.py $RELEASE_VERSION ./project/cytoplot/_version.py
 sed -i "s/dynamic = \[\x22version\x22\]/version=\x22${RELEASE_VERSION}\x22/" ./project/pyproject.toml
 sed -i "s/\[build-system\]/\[no-build-system]/" ./project/pyproject.toml
